b81597837e6a7f1f6ffeb620684a0125c41d3882
Teraguchi Norikazu <takami373@gmail.com>
2013-08-30 15:39:24 +0900

    jsonファイル作成し始め

---
+++ links.js
@@ -0,0 +1,4 @@
+var links = [
+		{ from: 0, to: 1},
+		{ from: 1, to: 2}
+	];
---
+++ point.js
@@ -0,0 +1,7 @@
+var initialPoint = 0;
+var currentPoint = 0;
+var points = [
+	{ x: 100, y:100, img:"aslfjks.jpg" },
+	{ x: 200, y:100, img:"assdass.jpg" },
+	{ x: 300, y:100, img:"asdajks.jpg" }
+];
