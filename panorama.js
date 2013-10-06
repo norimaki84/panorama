5ce1f8b4c32f580df3b68bebeb5dd078ddf8d40e
Teraguchi Norikazu <takami373@gmail.com>
2013-10-06 17:42:18 +0900

    左右の移動フラグを追加

--- panorama.js
+++ panorama.js
@@ -23,16 +23,16 @@ window.onload = function () {
 	fov = 72;
 	//                                  　　　　　　　　 画角１A 　　　　　　　　　アスペクト比１A
 	camera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, 0.1, 10000);
-	   
-	camera.position = new THREE.Vector3(0, 1, -1);
-	camera.target = new THREE.Vector3( 0, 0, 0 );
+	 
+	camera.position = new THREE.Vector3(0, 1, -1);   //カメラの初期位置
+	camera.target = new THREE.Vector3( 0, 0, 0 ); //カメラの注視点
 	camera.lookAt(camera.target);
 	scene.add(camera);
 	
 	ambient = new THREE.AmbientLight(0xFFFFFF);
 	scene.add(ambient);
 	
-	// 表示する物体の作成
+	// 初期位置の物体の作成
 	radius = 1;
 	segmentsWidth = 32;
 	segmentsHeight = 16;
@@ -59,15 +59,15 @@ window.onload = function () {
 	material01.side = THREE.BackSide;
 	mesh01 = new THREE.Mesh(geometry01, material01);
 	
-	mesh01.position.x=0;
-	mesh01.position.y=0;
-	mesh01.position.z=0;
+	mesh01.position.x=point[start].x;
+	mesh01.position.y=point[start].y;
+	mesh01.position.z=point[start].z;
 	
 	scene.add(mesh01);
 	
 	
 	
-	
+	//移動先の物体生成
 	var creategeometry = function (){
 		geometry02 = new THREE.SphereGeometry(
 			radius,
@@ -77,7 +77,7 @@ window.onload = function () {
 			phiLength,
 			thetaStart,
 			thetaLength
-		);
+		 );
 		material02 = new THREE.MeshBasicMaterial({
 			overdraw: true,
 			map: THREE.ImageUtils.loadTexture('images/2222.jpg', new THREE.UVMapping(), function() { 
@@ -87,13 +87,13 @@ window.onload = function () {
 				mesh02.position.x=point[next].x;
 				mesh02.position.y=point[next].y;
 				mesh02.position.z=point[next].z;
-	
+
 				scene.add(mesh02);
 				loadingFlag = false;
 				moveFlag = true;
-			})
+			});
 		});
-	}
+	};
 	
 	
 	
@@ -104,7 +104,7 @@ window.onload = function () {
 	now =0;
 	next = 1;
 	t =0;
-	//毎フレーム
+	
 	while(t < duration){
 				x =( point[now].x + (point[next].x - point[now].x)) * t / duration;
 				y =( point[now].y + (point[next].y - point[now].y)) * t / duration;
@@ -120,18 +120,18 @@ window.onload = function () {
 	//ポイント(カメラの位置、注視点)移動
 	document.onkeydown = function(e) { 
 		if (keychar == "x") {　//キーを押したとき
-			
 			if(moveFlag == false && loadingFlag == false){
 				if(create === true){
 					loadingFlag = true;
 					//物体生成
 					creategeometry();
-					
-				}
-	} 
-	
-	
-	
+					if(rightmoveFlag === true && leftmoveFlag === true){
+						loadingFlag = false;
+						 moveFlag = false;
+					};
+				};
+			};
+		}; 
 	
 	//レンダリング
 	baseTime = +new Date;
@@ -145,27 +145,27 @@ window.onload = function () {
 
 	function onWindowResize() {
 		camera.aspect = window.innerWidth / window.innerHeight;
-	      camera.updateProjectionMatrix();
-	      renderer.setSize( window.innerWidth, window.innerHeight );
+		camera.updateProjectionMatrix();
+		renderer.setSize( window.innerWidth, window.innerHeight );
 	};
 
 
 	function onDocumentMouseDown( event ) {
 		event.preventDefault();
-	    	isUserInteracting = true;
-	    	onPointerDownPointerX = event.clientX;
-	    	onPointerDownPointerY = event.clientY;
-	    	onPointerDownLon = lon;
-	    	onPointerDownLat = lat;
+			isUserInteracting = true;
+			onPointerDownPointerX = event.clientX;
+			onPointerDownPointerY = event.clientY;
+			onPointerDownLon = lon;
+			onPointerDownLat = lat;
 
 
-	    	lat = Math.max( - 85, Math.min( 85, lat ) );
-	    	phi = ( 90 - lat ) * Math.PI / 180;
-	    	theta = lon * Math.PI / 180;
-	    	camera.target.x = 500 * Math.sin( phi ) * Math.cos( theta );
-	    	camera.target.y = 500 * Math.cos( phi );
-	    	camera.target.z = 500 * Math.sin( phi ) * Math.sin( theta );
-	    	camera.lookAt( camera.target );
+			lat = Math.max( - 85, Math.min( 85, lat ) );
+			phi = ( 90 - lat ) * Math.PI / 180;
+			theta = lon * Math.PI / 180;
+			camera.target.x = 500 * Math.sin( phi ) * Math.cos( theta );
+			camera.target.y = 500 * Math.cos( phi );
+			camera.target.z = 500 * Math.sin( phi ) * Math.sin( theta );
+			camera.lookAt( camera.target );
 	};
 
 	function onDocumentMouseMove( event ) {
@@ -174,13 +174,13 @@ window.onload = function () {
 	      lat = ( event.clientY - onPointerDownPointerY ) * 0.1 + onPointerDownLat;
 		}
 
-	    	lat = Math.max( - 85, Math.min( 85, lat ) );
-	    	phi = ( 90 - lat ) * Math.PI / 180;
-	    	theta = lon * Math.PI / 180;
-	    	camera.target.x = 500 * Math.sin( phi ) * Math.cos( theta );
-	    	camera.target.y = 500 * Math.cos( phi );
-	    	camera.target.z = 500 * Math.sin( phi ) * Math.sin( theta );
-	    	camera.lookAt( camera.target );
+		lat = Math.max( - 85, Math.min( 85, lat ) );
+	 	phi = ( 90 - lat ) * Math.PI / 180;
+		theta = lon * Math.PI / 180;
+		camera.target.x = 500 * Math.sin( phi ) * Math.cos( theta );
+	 	camera.target.y = 500 * Math.cos( phi );
+	 	camera.target.z = 500 * Math.sin( phi ) * Math.sin( theta );
+		camera.lookAt( camera.target );
 	};
 
 	function onDocumentMouseUp( event ) {
