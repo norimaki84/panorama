59296f300a3c85f9d437e67ede47d11a72bccff8
Teraguchi Norikazu <takami373@gmail.com>
2013-10-06 17:45:06 +0900

    html→jsonファイル読み込みコード追加 その他jsonファイルは部分修正

---
+++ panorama_viewer.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function() {
+	var mapsFile = 'maps.json';
+	var linksFile = 'links.json';
+	var pointsFile = 'points.json';
+	var dateFile = 'date.json';
+	var modalsFile = 'modals.json';
+
+	jQuery.getJSON(mapsFile, { format: 'json' }, function(json) {
+		maps = json;
+		jQuery.getJSON(linksFile, { format: 'json' }, function(json) {
+			links = json;
+			jQuery.getJSON(pointsFile, { format: 'json' }, function(json) {
+				points = json;
+				jQuery.getJSON(dateFile, { format: 'json' }, function(json) {
+					date = json;
+					jQuery.getJSON(modalsFile, { format: 'json' }, function(json) {
+						modals = json;
+					initPanorama();
+					});
+				});
+			});
+		});
+	});
+});
+
+jQuery(window).load(function() {
+
+});
+
+var initPanorama = function() {
+	construct(texts); //??
+	view_construct(); //??
+	init_help(); //??
+};
