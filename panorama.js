fcf33b4c209482f0f0a1aae4d45bdb73a6c78db3
Teraguchi Norikazu <takami373@gmail.com>
2014-01-28 13:19:17 +0900

    変数宣言部分変更(リンク先ポインタ追加中)

--- panorama.js
+++ panorama.js
@@ -73,20 +73,27 @@ removeMesh = function (mesh) {
 createAllow = function(index){
     'use strict';
 
-    var geometry01 = new THREE.CubeGeometry(0.3, 0.2, 0.9); // 立方体作成01
-    var material01 = new THREE.MeshBasicMaterial({color: 0x0000aa}); // 材質作成
-    var mesh01     = new THREE.Mesh(geometry01, material01); // 立方体01と材質を結びつけてメッシュ作成
+    var geometry01, geometry02,
+        material01, material02,
+        mesh01, mesh02,
+        cube01, cube02,
+        scene,
+        group;
+
+    geometry01 = new THREE.CubeGeometry(0.3, 0.2, 0.9); // 立方体作成01
+    material01 = new THREE.MeshBasicMaterial({color: 0x0000aa}); // 材質作成
+    mesh01     = new THREE.Mesh(geometry01, material01); // 立方体01と材質を結びつけてメッシュ作成
     mesh01.position = new THREE.Vector3(-0.3, 0, 0);
-    var cube01 = mesh01;
+    cube01 = mesh01;
 
-    var geometry02 = new THREE.CubeGeometry(0.9, 0.2, 0.3); // 立方体作成02
-    var material02 = new THREE.MeshBasicMaterial({color: 0x0000aa}); // 材質作成
-    var mesh02     = new THREE.Mesh(geometry02, material02); // 立方体02と材質を結びつけてメッシュ作成
+    geometry02 = new THREE.CubeGeometry(0.9, 0.2, 0.3); // 立方体作成02
+    material02 = new THREE.MeshBasicMaterial({color: 0x0000aa}); // 材質作成
+    mesh02     = new THREE.Mesh(geometry02, material02); // 立方体02と材質を結びつけてメッシュ作成
     mesh02.position = new THREE.Vector3(0, 0, -0.3);
-    var cube02 = mesh02;
+    cube02 = mesh02;
 
-    var scene    = new THREE.Scene(); // シーン作成
-    var group = new THREE.Object3D();
+    scene    = new THREE.Scene(); // シーン作成
+    group = new THREE.Object3D();
     group.add(cube01);
     group.add(cube02);
 
