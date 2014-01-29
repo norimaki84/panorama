cb7a53a1747f9507f787d1ff9090c483d27433da
Teraguchi Norikazu <takami373@gmail.com>
2014-01-29 17:49:31 +0900

    カメラポジションを０にし、ポインタの位置を変更

--- allow.js
+++ allow.js
@@ -1,28 +1,30 @@
 var width = 640;
 var height = 480;
 
-var geometry01 = new THREE.CubeGeometry(0.3, 0.2, 0.9); // 立方体作成
+var geometry01 = new THREE.CubeGeometry(0.03, 0.02, 0.09); // 立方体作成
 var material01 = new THREE.MeshBasicMaterial({color: 0x0000aa}); // 材質作成
 var mesh01     = new THREE.Mesh(geometry01, material01); // 立方体と材質を結びつけてメッシュ作成
-mesh01.position = new THREE.Vector3(-0.3, 0, 0);
+mesh01.position = new THREE.Vector3(-0.03, 0, 0);
 var cube01 = mesh01;
 
-var geometry02 = new THREE.CubeGeometry(0.9, 0.2, 0.3); // 立方体作成
+var geometry02 = new THREE.CubeGeometry(0.09, 0.02, 0.03); // 立方体作成
 var material02 = new THREE.MeshBasicMaterial({color: 0x0000aa}); // 材質作成
 var mesh02     = new THREE.Mesh(geometry02, material02); // 立方体と材質を結びつけてメッシュ作成
-mesh02.position = new THREE.Vector3(0, 0, -0.3);
+mesh02.position = new THREE.Vector3(0, 0, -0.03);
 var cube02 = mesh02;
 
 var camera   = new THREE.PerspectiveCamera(40, width / height, 1, 1000); // カメラ作成。画角40、距離1〜1000の部分を表示できる。
-camera.position.x = 3;
-camera.position.y = 3;
-camera.position.z = 3;
-camera.lookAt(new THREE.Vector3(0, 0, 0)); 
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 0;
+camera.lookAt(new THREE.Vector3(1, -0.2, 1)); 
 
 var scene    = new THREE.Scene(); // シーン作成
 var group = new THREE.Object3D();
 group.add(cube01);
 group.add(cube02);
+
+group.position = new THREE.Vector3(0.8, -0.4, 0.8);
 
 scene.add(group); // シーンにメッシュ追加
 
